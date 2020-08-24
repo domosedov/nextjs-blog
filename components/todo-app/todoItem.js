515529c542b368268c1d4f1a7df759ed0c2169ff
Aleksandr Grigorii <domosedov.dev@gmail.com>
2020-08-24 11:05:12 +0300

    add telegram bot

--- components/todo-app/todoItem.js
+++ components/todo-app/todoItem.js
@@ -3,9 +3,30 @@ import React from 'react';
 const TodoItem = ({title}) => {
   return (
     <div
-      className="px-2 py-2 border"
+      className="px-2 py-2 border rounded"
     >
-      {title}
+      <div className="flex items-start justify-between">
+        <div>
+          {title}
+        </div>
+        <div className="inline-flex items-center space-x-1">
+          <button className="px-2 py-2 bg-white rounded-full border-2 border-indigo-500 text-indigo-500 duration-200 hover:border-green-500 hover:bg-green-500 hover:text-white focus:outline-none focus:shadow-outline">
+            <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/>
+            </svg>
+          </button>
+          <button className="px-2 py-2 bg-white rounded-full border-2 border-indigo-500 text-indigo-500 duration-200 hover:border-orange-400 hover:bg-orange-400 hover:text-white focus:outline-none focus:shadow-outline">
+            <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/>
+            </svg>
+          </button>
+          <button className="px-2 py-2 bg-white rounded-full border-2 border-indigo-500 text-indigo-500 duration-200 hover:border-red-600 hover:bg-red-600 hover:text-white focus:outline-none focus:shadow-outline">
+            <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z"/>
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,24 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios';
+import { error } from "next/dist/build/output/log";
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+export default async (req, res) => {
+  const METHOD = 'sendMessage';
+  const url = `${process.env.TELEGRAM_API_URL}bot${process.env.TELEGRAM_TOKEN}/${METHOD}?chat_id=${process.env.TELEGRAM_CHAT_ID}&parse_mode=html&text=`;
+  const { body } = req;
+  if (body) {
+    let data = '';
+    for (const [key, value] of Object.entries(body)) {
+      data += `<b>${key}:</b> ${value}\n`;
+    }
+    try {
+      const result = await axios.get(url + encodeURIComponent(data));
+      res.statusCode = 200;
+      res.json(result.data);
+    } catch (err) {
+      res.statusCode = 400;
+      res.json(err.response.data);
+    }
+  }
 }
+
+// https://api.telegram.org/bot1251929263:AAGdopBMaHizooBAIzNQjhe4tritZ2oLJX4/getUpdates
