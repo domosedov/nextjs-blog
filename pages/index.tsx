0c7b36c693590b47dc8ec7639daac9aa2647fa0f
Aleksandr Grigorii <domosedov.dev@gmail.com>
2020-12-02 18:07:04 +0300

    Update Resume v0.0.2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
-    <div className="flex items-center justify-center dark:bg-gray-800 dark:text-white">
-      <div>Hello</div>
+    <div className="flex items-center justify-center dark:bg-gray-800">
+      <div className="text-xl md:text-6xl bg-yellow-200  p-4 text-center rounded-lg shadow-lg m-4">
+        В процессе разработки.
+      </div>
     </div>
   );
 };
