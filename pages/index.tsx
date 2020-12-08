837d31ae54ffaf082b483dfd0f9dd4ef72b7dbab
Aleksandr Grigorii <domosedov.dev@gmail.com>
2020-12-08 12:27:52 +0300

    add resume link on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
+import Link from "next/link";
+
 const Home = () => {
   return (
     <div className="flex items-center justify-center dark:bg-gray-800">
-      <div className="text-xl md:text-6xl bg-yellow-200  p-4 text-center rounded-lg shadow-lg m-4">
-        В процессе разработки.
+      <div className="text-xl md:text-6xl bg-yellow-200 p-4 text-center rounded-lg shadow-lg m-4">
+        <p>В процессе разработки.</p>
+        <p>
+          Ознакомится с моим резюме можно{" "}
+          <Link href="/resume">
+            <a className="text-indigo-700 theme-focus">здесь</a>
+          </Link>
+          .
+        </p>
       </div>
     </div>
   );
